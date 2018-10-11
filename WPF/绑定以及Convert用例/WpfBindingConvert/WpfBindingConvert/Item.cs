efb3cab4b39c5799479fa42b3a9419d9bc35d3e3
Zjw <762259059@qq.com>
2018-10-11 20:23:20 +0800

    wpf绑定以及convert

---
+++ WPF/绑定以及Convert用例/WpfBindingConvert/WpfBindingConvert/Item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfBindingConvert
+{
+    public class Item: NotifyPropetryChanged
+    {
+        private int _color;
+        public int Color
+        {
+            get
+            {
+                return _color;
+            }
+            set
+            {
+                _color = value;
+                OnPropertyChanged("Color");
+            }
+        }
+
+    }
+}
---
+++ WPF/绑定以及Convert用例/WpfBindingConvert/WpfBindingConvert/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfBindingConvert
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Item item = new Item();
+        public MainWindow()
+        {
+            InitializeComponent();
+            item.Color = 1;
+            TxtBox.DataContext = item;
+            labTest.DataContext = item;
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            item.Color++;
+            item.Color = item.Color % 5;
+        }
+
+        private void BtnSub_Click(object sender, RoutedEventArgs e)
+        {
+            item.Color--;
+            if (item.Color == -1)
+            {
+                item.Color = 4;
+            }
+        }
+    }
+}
