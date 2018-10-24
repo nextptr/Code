e7828b006de417ea50e82a6f2c5bfb4524388683
Zjw <762259059@qq.com>
2018-10-24 19:12:46 +0800

    元胞自动机

---
+++ WPF/元胞自动机/WpfLifeGame/Cells.cs
@@ -0,0 +1,356 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace WpfLifeGame
+{
+    public class NotifyPropertyChanged : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool SetField<T>(ref T field, T value, string propertyName)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+            {
+                return false;
+            }
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+    public class cell : NotifyPropertyChanged
+    {
+        public bool LiveNext;
+        protected bool _now;
+        public cell()
+        {
+            LiveNext = false;
+            _now = false;
+        }
+
+        public bool LiveNow
+        {
+            set
+            {
+                _now = value; OnPropertyChanged("LiveNow");
+
+            }
+            get
+            {
+                return _now;
+            }
+        }
+    }
+
+    
+
+    public class Cells
+    {
+        public List<List<cell>> cells = new List<List<cell>>();
+        private DispatcherTimer _time = new DispatcherTimer();
+
+
+        public bool IsWork = false;
+        public int Count;
+        public Cells(int count)
+        {
+            //计时器
+            _time.Tick += _time_Tick; ;
+            _time.Interval = new TimeSpan(0, 0, 0, 0, 300);
+            _time.Start();
+            //cell
+            Count = count;
+            for (int i = 0; i < count; i++)
+            {
+                cells.Add(new List<cell>());
+                for (int j = 0; j < count; j++)
+                {
+                    cells[i].Add(new cell());
+                }
+            }
+        }
+
+        private void _time_Tick(object sender, EventArgs e)
+        {
+            if (true == IsWork)
+            {
+                Computer();
+                Flash();
+            }
+        }
+
+        public void Flash()
+        {
+            for(int i = 0;i<Count;i++)
+            {
+                for (int j = 0; j < Count; j++)
+                {
+                    cells[i][j].LiveNow = cells[i][j].LiveNext;
+                }
+            }
+        }
+
+        public void Start()
+        {
+            IsWork = true;
+        }
+
+        public void Stop()
+        {
+            IsWork = false;
+        }
+
+        public void ReSet()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                for (int j = 0; j < Count; j++)
+                {
+                    cells[i][j].LiveNow = false;
+                    cells[i][j].LiveNext = false;
+                }
+            }
+        }
+
+        private void Computer()
+        {
+            int row = 0;
+            int col = 0;
+            int naber = 0;
+            //00点
+            if (cells[0][1].LiveNow)
+            {
+                naber++;
+            }
+            if (cells[1][1].LiveNow)
+            {
+                naber++;
+            }
+            if (cells[1][0].LiveNow)
+            {
+                naber++;
+            }
+            core(0, 0, naber);
+            //01点
+            naber = 0;
+            if (cells[0][Count - 2].LiveNow)
+            {
+                naber++;
+            }
+            if (cells[1][Count - 2].LiveNow)
+            {
+                naber++;
+            }
+            if (cells[1][Count - 1].LiveNow)
+            {
+                naber++;
+            }
+            core(0, Count-1, naber);
+            //10
+            naber = 0;
+            if (cells[Count - 2][0].LiveNow)
+            {
+                naber++;
+            }
+            if (cells[Count - 2][1].LiveNow)
+            {
+                naber++;
+            }
+            if (cells[Count - 1][1].LiveNow)
+            {
+                naber++;
+            }
+            core(Count-1, 0, naber);
+            //11
+            naber = 0;
+            if (cells[Count - 1][Count - 2].LiveNow)
+            {
+                naber++;
+            }
+            if (cells[Count - 2][Count - 2].LiveNow)
+            {
+                naber++;
+            }
+            if (cells[Count - 2][Count - 1].LiveNow)
+            {
+                naber++;
+            }
+            core(Count-1, Count-1, naber);
+            //上边
+            for (int i = 1; i < Count - 1; i++)
+            {
+                naber = 0;
+                if (cells[0][i - 1].LiveNow)
+                {
+                    naber++;
+                }
+                if (cells[0][i + 1].LiveNow)
+                {
+                    naber++;
+                }
+                if (cells[1][i - 1].LiveNow)
+                {
+                    naber++;
+                }
+                if (cells[1][i].LiveNow)
+                {
+                    naber++;
+                }
+                if (cells[1][i + 1].LiveNow)
+                {
+                    naber++;
+                }
+                core(0, i, naber);
+            }
+            //下边
+            for (int i = 1; i < Count - 1; i++)
+            {
+                naber = 0;
+                if (cells[Count-1][i - 1].LiveNow)
+                {
+                    naber++;
+                }
+                if (cells[Count - 1][i + 1].LiveNow)
+                {
+                    naber++;
+                }
+                if (cells[Count - 2][i - 1].LiveNow)
+                {
+                    naber++;
+                }
+                if (cells[Count - 2][i].LiveNow)
+                {
+                    naber++;
+                }
+                if (cells[Count - 2][i + 1].LiveNow)
+                {
+                    naber++;
+                }
+                core(Count-1, i, naber);
+            }
+            //左边
+            for (int i = 1; i < Count - 1; i++)
+            {
+                naber = 0;
+                if (cells[i - 1][0].LiveNow)
+                {
+                    naber++;
+                }
+                if (cells[i + 1][0].LiveNow)
+                {
+                    naber++;
+                }
+                if (cells[i - 1][1].LiveNow)
+                {
+                    naber++;
+                }
+                if (cells[i][1].LiveNow)
+                {
+                    naber++;
+                }
+                if (cells[i + 1][1].LiveNow)
+                {
+                    naber++;
+                }
+                core(i, 0, naber);
+            }
+            //右边
+            for (int i = 1; i < Count - 1; i++)
+            {
+                naber = 0;
+                if (cells[i - 1][Count-1].LiveNow)
+                {
+                    naber++;
+                }
+                if (cells[i + 1][Count - 1].LiveNow)
+                {
+                    naber++;
+                }
+                if (cells[i - 1][Count - 2].LiveNow)
+                {
+                    naber++;
+                }
+                if (cells[i][Count - 2].LiveNow)
+                {
+                    naber++;
+                }
+                if (cells[i + 1][Count - 2].LiveNow)
+                {
+                    naber++;
+                }
+
+                core(i, Count-1, naber);
+            }
+            //正常
+            for (row=1; row < Count-1; row++)
+            {
+                for (col = 1; col < Count-1; col++)
+                {
+                    naber = 0;
+                    if (cells[row - 1][col - 1].LiveNow)
+                    {
+                        naber++;
+                    }
+                    if (cells[row - 1][col].LiveNow)
+                    {
+                        naber++;
+                    }
+                    if (cells[row - 1][col + 1].LiveNow)
+                    {
+                        naber++;
+                    }
+                    if (cells[row][col - 1].LiveNow)
+                    {
+                        naber++;
+                    }
+                    if (cells[row][col + 1].LiveNow)
+                    {
+                        naber++;
+                    }
+                    if (cells[row + 1][col - 1].LiveNow)
+                    {
+                        naber++;
+                    }
+                    if (cells[row + 1][col].LiveNow)
+                    {
+                        naber++;
+                    }
+                    if (cells[row + 1][col + 1].LiveNow)
+                    {
+                        naber++;
+                    }
+                    core(row, col, naber);
+                }
+            }
+        }
+
+        private void core(int x,int y ,int num)
+        {
+            if (num == 2)
+            {
+                cells[x][y].LiveNext = cells[x][y].LiveNow;
+            }
+            else if (num == 3)
+            {
+                cells[x][y].LiveNext = true;
+            }
+            else
+            {
+                cells[x][y].LiveNext = false;
+            }
+        }
+    }
+}
---
+++ WPF/元胞自动机/WpfLifeGame/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfLifeGame
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Cells cells;
+        public MainWindow()
+        {
+            InitializeComponent();
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            this.Loaded += MainWindow_Loaded;
+            btn_Start_stop.Click += Btn_Start_stop_Click;
+            btn_ReStart.Click += Btn_ReStart_Click;
+        }
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            InitCells(50);
+            InitBinding(50);
+        }
+
+        private void Btn_ReStart_Click(object sender, RoutedEventArgs e)
+        {
+            cells.Stop();
+            cells.ReSet();
+            btn_Start_stop.Content = "启动";
+        }
+
+        private void Btn_Start_stop_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn.Content.ToString() == "启动")
+            {
+                btn.Content = "暂停";
+                cells.Start();
+            }
+            else
+            {
+                btn.Content = "启动";
+                cells.Stop();
+            }
+        }
+
+        public void InitCells(int count)
+        {
+            //动态生成布局器
+            for (int i = 0; i < count; i++)
+            {
+                RowDefinition rd = new RowDefinition();
+                rd.Height = new GridLength();
+                gridboard.RowDefinitions.Add(rd);
+            }
+            for (int i = 0; i < count; i++)
+            {
+                ColumnDefinition cd = new ColumnDefinition();
+                cd.Width = new GridLength();
+                gridboard.ColumnDefinitions.Add(cd);
+            }
+            //初始化cell
+            cells = new Cells(count);
+        }
+
+        public void InitBinding(int count)
+        {
+            int wid = (int)gridboard.ActualHeight/ count;
+            for (int i = 0; i < count; i++)
+            {
+                for (int j = 0; j < count; j++)
+                {
+                    //动态绑定
+                    Binding binding = new Binding();
+                    binding.Source = cells.cells[i][j];
+                    binding.Path = new PropertyPath("LiveNow");
+                    binding.Mode = BindingMode.TwoWay;
+
+                    ToggleButton btn = new ToggleButton();
+                    btn.Style= this.FindResource("GreenToggleButton") as Style;
+                    btn.ApplyTemplate();
+                    btn.Height = wid;
+                    btn.Width = wid;
+                    btn.Tag = i.ToString() + "_" + j.ToString();
+                    btn.SetBinding(ToggleButton.IsCheckedProperty, binding);
+
+                    gridboard.Children.Add(btn);
+                    Grid.SetColumn(btn, i);
+                    Grid.SetRow(btn, j);
+                }
+            }
+        }
+
+    }
+}
